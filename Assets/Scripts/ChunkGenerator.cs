8fb2c049c1ffe2e06fae8424823b4c0e630e19f5
Jordan Tower <jordan@morxmedia.com>
2014-04-26 15:50:29 -0600

    fixe Gen bug

--- Assets/Scripts/ChunkGenerator.cs
+++ Assets/Scripts/ChunkGenerator.cs
@@ -10,8 +10,10 @@ public class ChunkGenerator : MonoBehaviour {
 	Vector2 lastchunk,currentchunk;
 	// Use this for initialization
 	void Start () {
-		
-		
+		Vector2 chunk_position = new Vector2(Mathf.FloorToInt(follow.transform.position.x/chunkSize),Mathf.FloorToInt(follow.transform.position.y/chunkSize));
+		GameObject.Instantiate(chunks[Random.Range(0,chunks.GetLength(0))],new Vector3((chunk_position.x)*chunkSize,chunk_position.y,0),Quaternion.identity);
+		GameObject.Instantiate(chunks[Random.Range(0,chunks.GetLength(0))],new Vector3((chunk_position.x+1)*chunkSize,chunk_position.y,0),Quaternion.identity);
+		GameObject.Instantiate(chunks[Random.Range(0,chunks.GetLength(0))],new Vector3((chunk_position.x-1)*chunkSize,chunk_position.y,0),Quaternion.identity);
 	}
 	
 	// Update is called once per frame
@@ -24,11 +26,11 @@ public class ChunkGenerator : MonoBehaviour {
 			Random.seed = (int)(chunk_position.x+chunk_position.y)+seed;
 			if(lastchunk.x>chunk_position.x)
 			{
-				GameObject.Instantiate(chunks[Random.Range(0,chunks.GetLength(0))],new Vector3(chunk_position.x*chunkSize,lastchunk.y,0),Quaternion.identity);
+				GameObject.Instantiate(chunks[Random.Range(0,chunks.GetLength(0))],new Vector3((chunk_position.x-1)*chunkSize,lastchunk.y,0),Quaternion.identity);
 			}
 			if(lastchunk.x<chunk_position.x)
 			{
-				GameObject.Instantiate(chunks[Random.Range(0,chunks.GetLength(0))],new Vector3(chunk_position.x*chunkSize,lastchunk.y,0),Quaternion.identity);
+				GameObject.Instantiate(chunks[Random.Range(0,chunks.GetLength(0))],new Vector3((chunk_position.x+1)*chunkSize,chunk_position.y,0),Quaternion.identity);
 			}
 		}
 		lastchunk = currentchunk;
