a4b075b1f5c7712a24b72d02a21f2957e73871bc
Exano <exanozz@gmail.com>
2014-04-27 01:40:21 -0400

    Fixed up bullet behavior.
    Improved bullets slightly.

--- Assets/Scripts/bulletBehavior.js
+++ Assets/Scripts/bulletBehavior.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 var timerScript : timerScript;
+var instantiated : boolean = false;
 
 function Start () {
 
@@ -10,17 +11,32 @@ function Update () {
 }
 
 function OnTriggerEnter(col:Collider){
-	if(col.collider.name == "1x1"){
-print("I hit: " + col.collider.name);
+	if(col.collider.name == "1x1" && !instantiated){ //make this layer based in the future, name based wont work for all the cool things we're adding.
+	//instantiate the new object
 	var newObject = Instantiate(col.collider.gameObject, col.collider.transform.position,Quaternion.identity);
+	//set up its name and physics plox
 	newObject.gameObject.AddComponent(Rigidbody);
 	newObject.gameObject.AddComponent("timerScript");
+	newObject.name = "cloneTerrain";
+	//good job, now apply constraints =p 	
+	newObject.rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+	//And destroy all the shit.
 	Destroy(col.collider.gameObject);
 	Destroy(transform.gameObject);
+	//dont accidently spawn another one, now!
+	instantiated = true;
+	
+//IF I code it right, this method should:
 //get the object
 //clone the object with physics
-//destroy the object
+//destroy the OG object
 //destroy the bullet.
+}
+else{//destroy bullet
+	Destroy(transform.gameObject);
+	if(col.collider.name == "cloneTerrain"){
+	col.transform.position.z-=0.1;
+	}
 }
 
 }
