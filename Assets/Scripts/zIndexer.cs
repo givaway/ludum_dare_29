140deffc21ee2a46fdf8047f83afdf10e77d5dd6
Exano <exanozz@gmail.com>
2014-04-26 22:43:49 -0400

    15 Chunks, Backgrounds
    Added 15 chunks, an interesting background, a few other things.

---
+++ Assets/Scripts/zIndexer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zIndexer : MonoBehaviour {
+	public int renderQueueIndex = 3000;
+	void Start () {
+		gameObject.renderer.material.renderQueue = renderQueueIndex;
+	}
+	
+	
+}
