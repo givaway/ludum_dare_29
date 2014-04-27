853970e9cc86d492ff715a2156451c16ee27c6cc
Exano <exanozz@gmail.com>
2014-04-27 02:10:56 -0400

    Minor chunk tweaks.
    Broke stallagtites, also! Isn't that awesome =D

---
+++ Assets/decideBackground.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+
+///All this script is responsible for is deciding wether or not the background elemts load. This can NOT go on the very last backgrounds, only BG 1 and 2.
+function Start () {
+	if(Random.Range(0,3)==1){
+		transform.renderer.enabled = false;
+	}
+}
