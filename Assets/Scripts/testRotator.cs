11a4c965182ea5fb0576352a3c46340b81dd6d14
Exano <exanozz@gmail.com>
2014-04-27 18:48:25 -0400

    Changed Pysics 2D? Added Rotation

---
+++ Assets/Scripts/testRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine; using System.Collections;
+
+public class testRotator : MonoBehaviour {
+	
+	public Transform myTransform; public float rotationSpeed = 100.0f; public int dir = 1;
+	
+	// Use this for initialization 
+	
+	void Start () { myTransform = transform; }
+	
+	// Update is called once per frame
+	
+	void Update () { 
+		myTransform.Rotate(0, Time.deltaTime *rotationSpeed * dir, Time.deltaTime *rotationSpeed * dir, Space.World); }
+	
+}
