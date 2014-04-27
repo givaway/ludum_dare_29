d05b62f5afad0db63f6bf81b694dde179da34dfd
Jordan Tower <jordan@morxmedia.com>
2014-04-27 16:33:19 -0600

    new scene

---
+++ Assets/Scripts/nebulageneration.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nebulageneration : MonoBehaviour {
+
+	public Texture[] textures;
+	public int density = 3;
+	GameObject[] g;
+	// Use this for initialization
+	void Start () {
+		Random.seed = Random.Range(0,53500);
+		g = new GameObject[density];
+		for(int i = 0; i<density; i++)
+		{
+			g[i] = (GameObject)GameObject.Instantiate(Resources.Load("NebulaTemp",typeof(GameObject)));
+			g[i].transform.eulerAngles = Random.insideUnitSphere*360;
+			g[i].renderer.material.mainTexture = textures[(int)Mathf.Floor(Random.Range(0,textures.GetLength(0)))];
+			Vector3 position = this.transform.position + (Random.insideUnitSphere*(this.transform.localScale.x/2));
+			g[i].transform.position = position;
+			g[i].transform.localScale = this.transform.localScale;
+			g[i].transform.parent = this.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject go in g)
+		{
+			go.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
+			Color sc = go.renderer.material.color;
+			sc.a = (Vector3.Distance(Camera.main.transform.position,this.transform.position)-Vector3.Distance(Camera.main.transform.position,go.transform.position))/this.transform.localScale.x;
+			go.renderer.material.color = sc;
+			
+		}
+	}
+}
