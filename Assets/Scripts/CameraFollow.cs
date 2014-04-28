54d99246b838a2ac2efcdf54876d4ffca1337110
Jordan Tower <jordan@morxmedia.com>
2014-04-27 22:47:11 -0600

    Fixed cam

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 	public GameObject follow;
 	// Use this for initialization
-	float velocity_ref = 0; 
+	float z;
+	Vector3 velocity_ref = Vector3.zero; 
 	void Start () {
-	
+		z = this.transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = this.transform.position;
-		pos.x = Mathf.SmoothDamp(pos.x,follow.transform.position.x,ref velocity_ref,0.4f);
-		//pos.y = Mathf.SmoothDamp(pos.y,follow.transform.position.y,ref velocity_ref,0.2f);
+		pos = Vector3.SmoothDamp(pos,follow.transform.position,ref velocity_ref,0.4f);
+		pos.z = z;
 		this.transform.position = pos;
 	}
 }
