fa5e4e9216322908c566e0a1ad79c119101fe5c0
Exano <exanozz@gmail.com>
2014-04-28 01:48:41 -0400

    Lots of things

--- Assets/Scripts/ChunkBehavior.cs
+++ Assets/Scripts/ChunkBehavior.cs
@@ -4,10 +4,17 @@ using System.Collections;
 public class ChunkBehavior : MonoBehaviour {
 
 	// Use this for initialization
+	public GameObject gemToMake;
 	public int radius = 0;
 	public GameObject follow; 
+	private GameObject ourGem;
 	void Start () {
-	
+
+		if(Random.Range(0,50)>=35){ 
+			 ourGem = (GameObject)GameObject.Instantiate(gemToMake,new Vector3(0f,0f,0f),Quaternion.identity);
+		   	 ourGem.transform.parent = transform;
+			 ourGem.transform.localPosition = new Vector3(Random.Range(-10,10),Random.Range(-10,10),-0.45f);
+		}
 	}
 	
 	// Update is called once per frame
