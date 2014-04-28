bcbb7f4f67729954d1328e9b8f3317f49a7d9c84
Exano <exanozz@gmail.com>
2014-04-28 17:49:45 -0400

    Randomized Lava Spawns/Z Pos slightly
    Just randomized it a bit more.

---
+++ Assets/lavaDecisionmaker.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+	if(Random.Range(0,5)>=4){
+		Destroy(transform.gameObject);
+	}
+	else{
+		if(Random.Range(0,5)>=4){
+			transform.localPosition.z = -2;
+		}
+	}
+	
+	
+}
